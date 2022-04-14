a6fb2468ecc171e588ef49a62e11ce48a85be872
sulavpo <sulupoudel.spp@gmail.com>
2022-04-14 07:04:27 +0545

    update 1

--- lib/landing_screen.dart
+++ lib/landing_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 
 class LandingScreen extends StatefulWidget {
@@ -12,64 +12,96 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+  int myIndex = 0;
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: LayoutBuilder(builder: (context, constraints){
-        return Container(
-          child: Column(
+      body: Stack(
+        children: [
+          Column(
             children: [
               Expanded(
-                flex: 4,
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      Container(
-                        color: Colors.blueGrey[800],
-                      ),
-                      Padding(padding: const EdgeInsets.all(5),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [Expanded(child: Text("Hi\nMorning'n Cameron!",style: textTheme.headline5!.apply(
-                              color: Colors.white,
-                            )),),
-                              Container(
-                                width: 140,
-                                height: 90,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Image.asset("lib/img/avatar.png"),
-                                ),
+                flex: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey[700]
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                            flex:1,
+                            child: Container(
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      flex:2,
+                                      child: Container(decoration: BoxDecoration(
+                                        //color: Colors.red
+                                      ),
+                                      )
+                                  ),
+                                  Expanded(
+                                      flex:1,
+                                      child: Container(decoration: BoxDecoration(
+                                       //color: Colors.blue
+                                      ),
+                                      )
+                                  )
+                                ],
                               ),
-
-                            ],
-                          ),
-                          Positioned(child:Container(
-                            width: constraints.maxHeight,
-                            height: constraints.maxWidth*0.35,
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              physics: BouncingScrollPhysics(),
+                              decoration: BoxDecoration(
+                                //color: Colors.white
                             ),
-                          )
-                          )
-                        ],
-                      ),)
-                    ],
-                  )),
-              Container(
-                width: constraints.maxHeight,
-                color: Colors.grey.shade200,
-                height: 440,
-              )
+                            )
+                        ),
+                        Expanded(
+                            flex:1,
+                            child: Container(decoration: BoxDecoration(
+                                //color: Colors.red
+                            ),
+                            )
+                        )
+                      ],
+                    ),
+                  )
+              ),
+              Expanded(
+                flex: 2,
+                  child: Container(
+
+                    decoration: BoxDecoration(
+                        color: Colors.blueGrey[900]
+                    ),
+                  )
+              ),
             ],
           ),
-        );
-      }),
+        ],
+
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.blueGrey[900],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 20),
+          child: GNav(
+            backgroundColor: Color(0xFF263238),
+            color: Colors.grey,
+            activeColor: Colors.grey,
+            tabBackgroundColor: Color(0xFF8E24AA),
+            gap: 8,
+            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+            tabs: const [
+              GButton(icon: Icons.home,text: 'Home',),
+              GButton(icon: Icons.add,text: 'New',),
+              GButton(icon: Icons.cloud,text: 'Cloud',),
+              GButton(icon: Icons.bar_chart,text: 'Status',),
+            ],
+          ),
+        ),
+      ),
+
     );
   }
   }
