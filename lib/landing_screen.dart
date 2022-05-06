82ff997b8ddb9d5823af6d1ec86d25cb2f603009
sulavpo <sulupoudel.spp@gmail.com>
2022-05-06 10:12:23 +0545

    model,listbuilder,list,constructor

--- lib/landing_screen.dart
+++ lib/landing_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:new_ui_app/header.dart';
+import 'package:new_ui_app/logo_scroll.dart';
 import 'package:new_ui_app/middle_part.dart';
 
 class LandingScreen extends StatefulWidget {
-  const LandingScreen({ Key? key }) : super(key: key);
+  const LandingScreen({Key? key}) : super(key: key);
 
   @override
   State<LandingScreen> createState() => _LandingScreenState();
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+  int _currentindex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,22 +48,50 @@ class _LandingScreenState extends State<LandingScreen> {
           ),
         ),
       ),
-    body: Column(
-      children: [Expanded(flex: 1,
-        child: Container(
-          child: Column(children: [Expanded(flex:1,
-          child: Container(child: Header(),)),
-          Expanded(flex: 1,
-            child: Container(
-            )),],
-            ),
-            )
-            ),
-          Expanded(flex: 2,
-          child: Container(
-            child: MiddlePart(),
-          ))],
-    ),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                child: Column(
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Header(),
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: ListView.builder(
+                            itemCount: logoPartList.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: LogoPart(
+                                  iconName:logoPartList[index].iconName,
+                                  title: logoPartList[index].title,
+                                  color: _currentindex == index
+                                      ? Colors.white
+                                      : Color.fromRGBO(38, 50, 56, 1),
+                                  onTap: () {
+                                    setState(() {
+                                      _currentindex = index;
+                                    });
+                                  },
+                                ),
+                              );
+                            })),
+                  ],
+                ),
+              )),
+          Expanded(
+              flex: 2,
+              child: Container(
+                child: MiddlePart(),
+              ))
+        ],
+      ),
       backgroundColor: Colors.blueGrey[700],
     );
   }
--- lib/logo_scroll.dart
+++ lib/logo_scroll.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
 
-class LogoPart extends StatefulWidget {
-  const LogoPart({ Key? key }) : super(key: key);
+ List<LogoPart> logoPartList = <LogoPart>[
 
-  @override
-  State<LogoPart> createState() => _LogoPartState();
-}
+	LogoPart( 
+		title:'Sunny',
+		iconName:Icons.sunny,
+    onTap:(){
+    } ,
+	),
+LogoPart(
+		title:'Night',
+		iconName:Icons.shield_moon,
+    onTap:(){} ,
+	),
+LogoPart(
+		title:'Music',
+		iconName:Icons.note,
+    onTap:(){} ,
+	),
+  LogoPart(
+		title:'Movie',
+		iconName:Icons.movie,
+    onTap:(){} ,
+	),
+  LogoPart(
+		title:'Leave',
+		iconName:Icons.lock,
+    onTap:(){} ,
+	),
+];
 
-class _LogoPartState extends State<LogoPart> {
+
+class LogoPart extends StatelessWidget {
+  final String title;
+  final IconData iconName;
+  final Color color;
+  final void Function() onTap;
+
+  const LogoPart(
+      {Key? key,
+      required this.title,
+      required this.iconName,
+      this.color = Colors.white,required this.onTap})
+      : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: 70,
+            height: 70,
+            child: Icon(
+             iconName,
+             size: 50,
+             color: Colors.yellow,
+            ),
+            decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+          ),
+          Text(title)
+        ],
+      ),
     );
   }
 }
